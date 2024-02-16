417472cac5a79b0ee76ea5c19e19396995bc2897
邱国宾 <qiugb@119.net>
2024-02-16 13:09:00 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloWorld).Methods("GET")
+
+	http.ListenAndServe(":8080", router)
+}
